817415cfe2647ef244b31d4c9271e26eb7bcb66f
lzx <lucaschang1918@gmail.com>
2025-10-24 22:02:43 +0800

    version 1.1

--- src/widget/ui_yolocontrol.h
+++ src/widget/ui_yolocontrol.h
@@ -34,7 +34,7 @@ public:
     QWidget *centralwidget;
     QHBoxLayout *horizontalLayout_5;
     QSplitter *splitter_2;
-    QWidget *widget;
+    QWidget *layoutWidget;
     QVBoxLayout *verticalLayout_3;
     QGridLayout *gridLayout_4;
     QLabel *label;
@@ -68,7 +68,7 @@ public:
     QPushButton *m_btn2Kmodel;
     QSpacerItem *horizontalSpacer_4;
     QSplitter *splitter;
-    QWidget *widget1;
+    QWidget *layoutWidget1;
     QVBoxLayout *verticalLayout;
     QSpacerItem *verticalSpacer;
     QLabel *label_11;
@@ -84,7 +84,7 @@ public:
     QPushButton *m_Dir2Kmodel;
     QSpacerItem *horizontalSpacer_6;
     QSpacerItem *verticalSpacer_2;
-    QWidget *widget2;
+    QWidget *layoutWidget2;
     QVBoxLayout *verticalLayout_4;
     QLabel *label_10;
     QTextEdit *m_editLog;
@@ -94,7 +94,7 @@ public:
     {
         if (yoloControl->objectName().isEmpty())
             yoloControl->setObjectName("yoloControl");
-        yoloControl->resize(1081, 353);
+        yoloControl->resize(1058, 353);
         centralwidget = new QWidget(yoloControl);
         centralwidget->setObjectName("centralwidget");
         horizontalLayout_5 = new QHBoxLayout(centralwidget);
@@ -102,20 +102,20 @@ public:
         splitter_2 = new QSplitter(centralwidget);
         splitter_2->setObjectName("splitter_2");
         splitter_2->setOrientation(Qt::Orientation::Horizontal);
-        widget = new QWidget(splitter_2);
-        widget->setObjectName("widget");
-        verticalLayout_3 = new QVBoxLayout(widget);
+        layoutWidget = new QWidget(splitter_2);
+        layoutWidget->setObjectName("layoutWidget");
+        verticalLayout_3 = new QVBoxLayout(layoutWidget);
         verticalLayout_3->setObjectName("verticalLayout_3");
         verticalLayout_3->setContentsMargins(0, 0, 0, 0);
         gridLayout_4 = new QGridLayout();
         gridLayout_4->setObjectName("gridLayout_4");
-        label = new QLabel(widget);
+        label = new QLabel(layoutWidget);
         label->setObjectName("label");
         label->setAlignment(Qt::AlignmentFlag::AlignLeading|Qt::AlignmentFlag::AlignLeft|Qt::AlignmentFlag::AlignVCenter);
 
         gridLayout_4->addWidget(label, 0, 0, 1, 2);
 
-        m_comVersion = new QComboBox(widget);
+        m_comVersion = new QComboBox(layoutWidget);
         m_comVersion->addItem(QString());
         m_comVersion->addItem(QString());
         m_comVersion->addItem(QString());
@@ -123,47 +123,47 @@ public:
 
         gridLayout_4->addWidget(m_comVersion, 0, 2, 1, 1);
 
-        label_9 = new QLabel(widget);
+        label_9 = new QLabel(layoutWidget);
         label_9->setObjectName("label_9");
         label_9->setAlignment(Qt::AlignmentFlag::AlignLeading|Qt::AlignmentFlag::AlignLeft|Qt::AlignmentFlag::AlignVCenter);
 
         gridLayout_4->addWidget(label_9, 1, 0, 1, 2);
 
-        m_comEvnName = new QComboBox(widget);
+        m_comEvnName = new QComboBox(layoutWidget);
         m_comEvnName->addItem(QString());
         m_comEvnName->setObjectName("m_comEvnName");
         m_comEvnName->setEditable(true);
 
         gridLayout_4->addWidget(m_comEvnName, 1, 2, 1, 2);
 
-        label_2 = new QLabel(widget);
+        label_2 = new QLabel(layoutWidget);
         label_2->setObjectName("label_2");
         label_2->setAlignment(Qt::AlignmentFlag::AlignLeading|Qt::AlignmentFlag::AlignLeft|Qt::AlignmentFlag::AlignVCenter);
 
         gridLayout_4->addWidget(label_2, 2, 0, 1, 1);
 
-        m_lineYoloPath = new QLineEdit(widget);
+        m_lineYoloPath = new QLineEdit(layoutWidget);
         m_lineYoloPath->setObjectName("m_lineYoloPath");
 
         gridLayout_4->addWidget(m_lineYoloPath, 2, 1, 1, 2);
 
-        m_btnYoloOpen = new QPushButton(widget);
+        m_btnYoloOpen = new QPushButton(layoutWidget);
         m_btnYoloOpen->setObjectName("m_btnYoloOpen");
 
         gridLayout_4->addWidget(m_btnYoloOpen, 2, 3, 1, 1);
 
-        label_3 = new QLabel(widget);
+        label_3 = new QLabel(layoutWidget);
         label_3->setObjectName("label_3");
         label_3->setAlignment(Qt::AlignmentFlag::AlignLeading|Qt::AlignmentFlag::AlignLeft|Qt::AlignmentFlag::AlignVCenter);
 
         gridLayout_4->addWidget(label_3, 3, 0, 1, 1);
 
-        m_lineYamlPath = new QLineEdit(widget);
+        m_lineYamlPath = new QLineEdit(layoutWidget);
         m_lineYamlPath->setObjectName("m_lineYamlPath");
 
         gridLayout_4->addWidget(m_lineYamlPath, 3, 1, 1, 2);
 
-        m_btnYamlOpen = new QPushButton(widget);
+        m_btnYamlOpen = new QPushButton(layoutWidget);
         m_btnYamlOpen->setObjectName("m_btnYamlOpen");
 
         gridLayout_4->addWidget(m_btnYamlOpen, 3, 3, 1, 1);
@@ -173,7 +173,7 @@ public:
 
         verticalLayout_2 = new QVBoxLayout();
         verticalLayout_2->setObjectName("verticalLayout_2");
-        label_4 = new QLabel(widget);
+        label_4 = new QLabel(layoutWidget);
         label_4->setObjectName("label_4");
         label_4->setFrameShadow(QFrame::Shadow::Sunken);
         label_4->setAlignment(Qt::AlignmentFlag::AlignCenter);
@@ -182,52 +182,52 @@ public:
 
         gridLayout_3 = new QGridLayout();
         gridLayout_3->setObjectName("gridLayout_3");
-        label_5 = new QLabel(widget);
+        label_5 = new QLabel(layoutWidget);
         label_5->setObjectName("label_5");
         label_5->setAlignment(Qt::AlignmentFlag::AlignLeading|Qt::AlignmentFlag::AlignLeft|Qt::AlignmentFlag::AlignVCenter);
 
         gridLayout_3->addWidget(label_5, 0, 0, 1, 1);
 
-        m_spinEpochs = new QSpinBox(widget);
+        m_spinEpochs = new QSpinBox(layoutWidget);
         m_spinEpochs->setObjectName("m_spinEpochs");
         m_spinEpochs->setMaximum(1000000);
         m_spinEpochs->setValue(250);
 
         gridLayout_3->addWidget(m_spinEpochs, 0, 1, 1, 1);
 
-        label_6 = new QLabel(widget);
+        label_6 = new QLabel(layoutWidget);
         label_6->setObjectName("label_6");
         label_6->setAlignment(Qt::AlignmentFlag::AlignLeading|Qt::AlignmentFlag::AlignLeft|Qt::AlignmentFlag::AlignVCenter);
 
         gridLayout_3->addWidget(label_6, 1, 0, 1, 1);
 
-        m_spinBatchSize = new QSpinBox(widget);
+        m_spinBatchSize = new QSpinBox(layoutWidget);
         m_spinBatchSize->setObjectName("m_spinBatchSize");
         m_spinBatchSize->setMaximum(1000000);
         m_spinBatchSize->setValue(8);
 
         gridLayout_3->addWidget(m_spinBatchSize, 1, 1, 1, 1);
 
-        label_7 = new QLabel(widget);
+        label_7 = new QLabel(layoutWidget);
         label_7->setObjectName("label_7");
         label_7->setAlignment(Qt::AlignmentFlag::AlignLeading|Qt::AlignmentFlag::AlignLeft|Qt::AlignmentFlag::AlignVCenter);
 
         gridLayout_3->addWidget(label_7, 2, 0, 1, 1);
 
-        m_spinImageSize = new QSpinBox(widget);
+        m_spinImageSize = new QSpinBox(layoutWidget);
         m_spinImageSize->setObjectName("m_spinImageSize");
         m_spinImageSize->setMaximum(1000000);
         m_spinImageSize->setValue(320);
 
         gridLayout_3->addWidget(m_spinImageSize, 2, 1, 1, 1);
 
-        label_8 = new QLabel(widget);
+        label_8 = new QLabel(layoutWidget);
         label_8->setObjectName("label_8");
         label_8->setAlignment(Qt::AlignmentFlag::AlignLeading|Qt::AlignmentFlag::AlignLeft|Qt::AlignmentFlag::AlignVCenter);
 
         gridLayout_3->addWidget(label_8, 3, 0, 1, 1);
 
-        m_comboDevice = new QComboBox(widget);
+        m_comboDevice = new QComboBox(layoutWidget);
         m_comboDevice->addItem(QString());
         m_comboDevice->addItem(QString());
         m_comboDevice->setObjectName("m_comboDevice");
@@ -246,12 +246,12 @@ public:
 
         gridLayout->addItem(horizontalSpacer, 0, 0, 1, 1);
 
-        m_btnTrainPt = new QPushButton(widget);
+        m_btnTrainPt = new QPushButton(layoutWidget);
         m_btnTrainPt->setObjectName("m_btnTrainPt");
 
         gridLayout->addWidget(m_btnTrainPt, 0, 1, 1, 1);
 
-        m_btnStop = new QPushButton(widget);
+        m_btnStop = new QPushButton(layoutWidget);
         m_btnStop->setObjectName("m_btnStop");
 
         gridLayout->addWidget(m_btnStop, 0, 2, 1, 1);
@@ -264,12 +264,12 @@ public:
 
         gridLayout->addItem(horizontalSpacer_3, 1, 0, 1, 1);
 
-        m_btn2Onnx = new QPushButton(widget);
+        m_btn2Onnx = new QPushButton(layoutWidget);
         m_btn2Onnx->setObjectName("m_btn2Onnx");
 
         gridLayout->addWidget(m_btn2Onnx, 1, 1, 1, 1);
 
-        m_btn2Kmodel = new QPushButton(widget);
+        m_btn2Kmodel = new QPushButton(layoutWidget);
         m_btn2Kmodel->setObjectName("m_btn2Kmodel");
 
         gridLayout->addWidget(m_btn2Kmodel, 1, 2, 1, 1);
@@ -281,20 +281,20 @@ public:
 
         verticalLayout_3->addLayout(gridLayout);
 
-        splitter_2->addWidget(widget);
+        splitter_2->addWidget(layoutWidget);
         splitter = new QSplitter(splitter_2);
         splitter->setObjectName("splitter");
         splitter->setOrientation(Qt::Orientation::Horizontal);
-        widget1 = new QWidget(splitter);
-        widget1->setObjectName("widget1");
-        verticalLayout = new QVBoxLayout(widget1);
+        layoutWidget1 = new QWidget(splitter);
+        layoutWidget1->setObjectName("layoutWidget1");
+        verticalLayout = new QVBoxLayout(layoutWidget1);
         verticalLayout->setObjectName("verticalLayout");
         verticalLayout->setContentsMargins(0, 0, 0, 0);
         verticalSpacer = new QSpacerItem(20, 40, QSizePolicy::Policy::Minimum, QSizePolicy::Policy::Expanding);
 
         verticalLayout->addItem(verticalSpacer);
 
-        label_11 = new QLabel(widget1);
+        label_11 = new QLabel(layoutWidget1);
         label_11->setObjectName("label_11");
         label_11->setAlignment(Qt::AlignmentFlag::AlignCenter);
 
@@ -302,32 +302,32 @@ public:
 
         gridLayout_2 = new QGridLayout();
         gridLayout_2->setObjectName("gridLayout_2");
-        label_12 = new QLabel(widget1);
+        label_12 = new QLabel(layoutWidget1);
         label_12->setObjectName("label_12");
 
         gridLayout_2->addWidget(label_12, 0, 0, 1, 2);
 
-        m_linePtPath = new QLineEdit(widget1);
+        m_linePtPath = new QLineEdit(layoutWidget1);
         m_linePtPath->setObjectName("m_linePtPath");
 
         gridLayout_2->addWidget(m_linePtPath, 0, 2, 1, 3);
 
-        m_btnPtOpen = new QPushButton(widget1);
+        m_btnPtOpen = new QPushButton(layoutWidget1);
         m_btnPtOpen->setObjectName("m_btnPtOpen");
 
         gridLayout_2->addWidget(m_btnPtOpen, 0, 5, 1, 1);
 
-        label_13 = new QLabel(widget1);
+        label_13 = new QLabel(layoutWidget1);
         label_13->setObjectName("label_13");
 
         gridLayout_2->addWidget(label_13, 1, 0, 1, 2);
 
-        m_lineValPath = new QLineEdit(widget1);
+        m_lineValPath = new QLineEdit(layoutWidget1);
         m_lineValPath->setObjectName("m_lineValPath");
 
         gridLayout_2->addWidget(m_lineValPath, 1, 2, 1, 3);
 
-        m_btnValOpen = new QPushButton(widget1);
+        m_btnValOpen = new QPushButton(layoutWidget1);
         m_btnValOpen->setObjectName("m_btnValOpen");
 
         gridLayout_2->addWidget(m_btnValOpen, 1, 5, 1, 1);
@@ -336,12 +336,12 @@ public:
 
         gridLayout_2->addItem(horizontalSpacer_5, 2, 0, 1, 1);
 
-        m_btnDir2Onnx = new QPushButton(widget1);
+        m_btnDir2Onnx = new QPushButton(layoutWidget1);
         m_btnDir2Onnx->setObjectName("m_btnDir2Onnx");
 
         gridLayout_2->addWidget(m_btnDir2Onnx, 2, 1, 1, 2);
 
-        m_Dir2Kmodel = new QPushButton(widget1);
+        m_Dir2Kmodel = new QPushButton(layoutWidget1);
         m_Dir2Kmodel->setObjectName("m_Dir2Kmodel");
 
         gridLayout_2->addWidget(m_Dir2Kmodel, 2, 3, 1, 1);
@@ -357,25 +357,25 @@ public:
 
         verticalLayout->addItem(verticalSpacer_2);
 
-        splitter->addWidget(widget1);
-        widget2 = new QWidget(splitter);
-        widget2->setObjectName("widget2");
-        verticalLayout_4 = new QVBoxLayout(widget2);
+        splitter->addWidget(layoutWidget1);
+        layoutWidget2 = new QWidget(splitter);
+        layoutWidget2->setObjectName("layoutWidget2");
+        verticalLayout_4 = new QVBoxLayout(layoutWidget2);
         verticalLayout_4->setObjectName("verticalLayout_4");
         verticalLayout_4->setContentsMargins(0, 0, 0, 0);
-        label_10 = new QLabel(widget2);
+        label_10 = new QLabel(layoutWidget2);
         label_10->setObjectName("label_10");
         label_10->setAlignment(Qt::AlignmentFlag::AlignCenter);
 
         verticalLayout_4->addWidget(label_10);
 
-        m_editLog = new QTextEdit(widget2);
+        m_editLog = new QTextEdit(layoutWidget2);
         m_editLog->setObjectName("m_editLog");
         m_editLog->setReadOnly(true);
 
         verticalLayout_4->addWidget(m_editLog);
 
-        splitter->addWidget(widget2);
+        splitter->addWidget(layoutWidget2);
         splitter_2->addWidget(splitter);
 
         horizontalLayout_5->addWidget(splitter_2);
@@ -417,8 +417,8 @@ public:
         m_btnStop->setText(QCoreApplication::translate("yoloControl", "\345\201\234\346\255\242\350\256\255\347\273\203", nullptr));
         m_btn2Onnx->setText(QCoreApplication::translate("yoloControl", "toOnnx", nullptr));
         m_btn2Kmodel->setText(QCoreApplication::translate("yoloControl", "toKmodel", nullptr));
-        label_11->setText(QCoreApplication::translate("yoloControl", "\344\270\215\350\256\255\347\273\203\347\233\264\346\216\245\350\275\254\345\214\226\351\234\200\350\246\201\346\217\220\344\276\233pt\346\226\207\344\273\266\345\222\214val\346\225\260\346\215\256\351\233\206", nullptr));
-        label_12->setText(QCoreApplication::translate("yoloControl", "pt\346\226\207\344\273\266\350\267\257\345\276\204:", nullptr));
+        label_11->setText(QCoreApplication::translate("yoloControl", "\344\270\215\350\256\255\347\273\203\347\233\264\346\216\245\350\275\254\345\214\226\351\234\200\350\246\201\346\217\220\344\276\233\346\250\241\345\236\213\346\226\207\344\273\266\345\222\214val\346\225\260\346\215\256\351\233\206\351\273\230\350\256\244\346\230\257best.onnx/best.pt", nullptr));
+        label_12->setText(QCoreApplication::translate("yoloControl", "\346\250\241\345\236\213\346\226\207\344\273\266\350\267\257\345\276\204:", nullptr));
         m_btnPtOpen->setText(QCoreApplication::translate("yoloControl", "\346\211\223\345\274\200", nullptr));
         label_13->setText(QCoreApplication::translate("yoloControl", "val\346\225\260\346\215\256\351\233\206\350\267\257\345\276\204:", nullptr));
         m_btnValOpen->setText(QCoreApplication::translate("yoloControl", "\346\211\223\345\274\200", nullptr));
